04957eca397d8c0a7573ab3f8c974dbf608bb314
wmingwei <wmingweiw@gmail.com>
2017-08-11 19:53:44 -0500

    add __init__.py

---
+++ RBM/__init__.py
@@ -0,0 +1,13 @@
+# from os.path import dirname, basename, isfile
+# import glob
+# modules = glob.glob(dirname(__file__)+"/*.py")
+# __all__ = [ basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
+
+# import RBM.model
+
+import os
+for module in os.listdir(os.path.dirname(__file__)):
+    if module == '__init__.py' or module[-3:] != '.py':
+        continue
+    __import__('RBM.'+module[:-3], locals(), globals())
+del module
